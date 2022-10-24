e08620ddb07514fc9c85da4b4aaa8f27db28496c
Agustin Di Nezio <adineziocamiletti@gmail.com>
2022-10-24 07:06:47 -0300

    Agregar archivos de proyecto.

---
+++ PracticaStruct/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PracticaStruct
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnIngresaDatos = new System.Windows.Forms.Button();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.lstImprimeDatos = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnIngresaDatos
+            // 
+            this.btnIngresaDatos.Location = new System.Drawing.Point(380, 55);
+            this.btnIngresaDatos.Name = "btnIngresaDatos";
+            this.btnIngresaDatos.Size = new System.Drawing.Size(235, 23);
+            this.btnIngresaDatos.TabIndex = 0;
+            this.btnIngresaDatos.Text = "Ingresa Datos";
+            this.btnIngresaDatos.UseVisualStyleBackColor = true;
+            this.btnIngresaDatos.Click += new System.EventHandler(this.btnIngresaDatos_Click);
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(380, 117);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(235, 23);
+            this.btnMostrar.TabIndex = 1;
+            this.btnMostrar.Text = "Mostrar Datos";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // lstImprimeDatos
+            // 
+            this.lstImprimeDatos.FormattingEnabled = true;
+            this.lstImprimeDatos.Location = new System.Drawing.Point(380, 181);
+            this.lstImprimeDatos.Name = "lstImprimeDatos";
+            this.lstImprimeDatos.Size = new System.Drawing.Size(235, 225);
+            this.lstImprimeDatos.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstImprimeDatos);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.btnIngresaDatos);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIngresaDatos;
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.ListBox lstImprimeDatos;
+    }
+}
+
---
+++ PracticaStruct/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace PracticaStruct
+{
+    public partial class Form1 : Form
+    {
+        public struct Alumno {
+            public string Nombre;
+            public double[] Notas;
+            public double PromedioNotas;
+            public double MayorNota;
+        }
+        Alumno[] estudiantes;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIngresaDatos_Click(object sender, EventArgs e)
+        {
+            estudiantes= new Alumno[4];
+            InicializaCampos(estudiantes);
+            CargaDatos(estudiantes);
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+            ImprimeDatos(estudiantes);
+        }
+
+        //----------------------------------------METODOS-----------------------------------------------------//
+        //----------------------------------------------------------------------------------------------------//
+
+        public void InicializaCampos(Alumno[] alumnos)
+        {
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                alumnos[i].Nombre = "";
+                alumnos[i].MayorNota = 0;
+                alumnos[i].Notas=new double[3];
+                alumnos[i].PromedioNotas = 0;
+            }
+        }
+
+        public string CargaNombre()
+        {
+            string nombre = Interaction.InputBox("Ingrese nombre", "Nombre Estudiante");
+            return nombre;
+        }
+
+        public void CargaNotas(double[] notas)
+        {
+            for (int i = 0; i < notas.Length; i++)
+            {
+                notas[i] = Convert.ToDouble(Interaction.InputBox("Ingrese nota", "Notas"));
+            }
+        }
+        public double Promedio(double[] notas)
+        {
+            double suma = 0;
+            for (int i = 0; i < notas.Length; i++)
+            {
+                suma += notas[i];
+            }
+            return suma / notas.Count();
+        }
+
+        public void CargaDatos(Alumno[] alumnos)
+        {
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                alumnos[i].Nombre = CargaNombre();
+                CargaNotas(alumnos[i].Notas);
+                alumnos[i].PromedioNotas=Promedio(alumnos[i].Notas);
+                alumnos[i].MayorNota = BuscaMayorNota(alumnos[i].Notas);
+            }
+        }
+        
+        public double BuscaMayorNota(double[] notas)
+        {
+            double mayor = notas[0];
+            for (int i = 1; i < notas.Length; i++)
+            {
+                if (notas[i] > mayor) mayor = notas[i];
+            }
+            return mayor;
+        }
+
+        public void MuestraNombre(string nombre)
+        {
+            lstImprimeDatos.Items.Add(nombre);
+        }
+
+        public void MuestraNotas(double[] notas)
+        {
+            for (int i = 0; i < notas.Length; i++)
+            {
+                lstImprimeDatos.Items.Add(notas[i]);
+            }
+        }
+
+        public void MuestraPromedioYMayorNota(Alumno alumnos)
+        {
+            
+                lstImprimeDatos.Items.Add($"La mayor nota de {alumnos.Nombre} es : {alumnos.MayorNota}");
+                lstImprimeDatos.Items.Add($"Promedio : {alumnos.PromedioNotas}");
+
+                if (alumnos.PromedioNotas < 4) lstImprimeDatos.Items.Add("Debes ir a recuperatorio !\n");
+                else if (alumnos.PromedioNotas >= 4 && alumnos.PromedioNotas <= 7) lstImprimeDatos.Items.Add("Bien !\n");
+                else if (alumnos.PromedioNotas > 7) lstImprimeDatos.Items.Add("Muy Bien !\n");
+                else lstImprimeDatos.Items.Add("Hubo un error!\n");
+            
+        }
+
+        public void ImprimeDatos(Alumno[] alumnos)
+        {
+            for (int i = 0; i < alumnos.Length; i++)
+            {
+                MuestraNombre(alumnos[i].Nombre);
+                MuestraNotas(alumnos[i].Notas);
+                MuestraPromedioYMayorNota(alumnos[i]);
+            }
+        }
+
+
+    }
+}
